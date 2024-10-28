21cc556fa0d95fdd6687fd1429c38f4e82432623
TANVEERHASSANGETHIB <bscs22144@itu.edu.pk>
2024-10-28 17:36:18 +0500

    Navbar Implemented

--- my-vite-app/src/Components/Navbar.jsx
+++ my-vite-app/src/Components/Navbar.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+ 
+ <div id="logo">
+      <img 
+        src='./download.png' 
+        alt='logo' 
+        style={{ marginRight: '20px', width: '150px', height: '50px' }} 
+      />
+      </div>
+      <ul style={{ listStyle: 'none', display: 'flex', margin: 0, padding: 0 }}>
+        <li style={{ marginRight: '15px' }}>Home</li>
+        <li style={{ marginRight: '15px' }}>Experiences</li>
+        <li style={{ marginRight: '15px' }}>Online Experiences</li>
+      </ul>
+      <input 
+        type="text" 
+        placeholder="Search..." 
+        style={{ marginLeft: '20px', padding: '5px' }} 
+      />
+    </div>
+  );
+};
