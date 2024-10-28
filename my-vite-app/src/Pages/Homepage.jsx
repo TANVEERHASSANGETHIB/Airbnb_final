4cbd93ff52b9a935794af144e14f8f25ecb76c23
TANVEERHASSANGETHIB <bscs22144@itu.edu.pk>
2024-10-28 17:16:51 +0500

    Components Initialized

---
+++ my-vite-app/src/Pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from '../Components/Navbar'
+import { Footer } from '../Components/Footer'
+export const Homepage = () => 
+{
+  return (
+    <div>
+        <Navbar/>
+        <Footer/>
+    </div>
+  )
+}
