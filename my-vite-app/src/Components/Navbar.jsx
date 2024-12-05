6f833233c4bf96f2f0f3edeb27a54107f61ab723
TANVEERHASSANGETHIB <bscs22144@itu.edu.pk>
2024-12-05 18:06:33 +0500

    Nav Bar Css Initial Phase Completed

--- my-vite-app/src/Components/Navbar.jsx
+++ my-vite-app/src/Components/Navbar.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import { Search } from './Search';
+import '../Style/Navbar.css';
 
 export const Navbar = () => {
   return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
- 
- <div id="logo">
-      <img  src='./download.png' alt='logo' style={{ marginRight: '20px', width: '150px', height: '50px' }} />
-      
+    <div id="navbar">
+      <div id="logo">
+        <img src='./download.png' alt='logo' />
       </div>
-      <ul style={{ listStyle: 'none', display: 'flex', margin: 0, padding: 0 }}>
-        <li style={{ marginRight: '15px' }}>Home</li>
-        <li style={{ marginRight: '15px' }}>Experiences</li>
-        <li style={{ marginRight: '15px' }}>Online Experiences</li>
-      </ul>
-      <Search/>
-      
+
+      <Search />
+
       <div id="usermenu">
-      <ul style={{ listStyle: 'none', display: 'flex', margin: 0, paddingLeft:90  }}>
-        <li style={{ marginRight: '15px' }}>Login</li>
-        <li style={{ marginRight: '15px' }}>Signup</li>
-      </ul>
+        <ul>
+          <li>Login</li>
+          <li>Signup</li>
+        </ul>
       </div>
     </div>
   );
--- my-vite-app/src/Components/Search.jsx
+++ my-vite-app/src/Components/Search.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 
 export const Search = () => {
   return (
-    <div id="search" style={{ display: 'flex', alignItems: 'center' }}>
-      <input 
-        type="text" 
-        placeholder="Search..." 
-        style={{ marginRight: '10px', padding: '5px' }} 
-      />
-      <div id="search_btn">   
-        <button>
-          Search
-        </button>
-      </div>
+    <div id="search">
+      <input type="text" placeholder="Where - Search destinations" />
+      <input type="text" placeholder="Check-in - Add dates" />
+      <input type="text" placeholder="Check-out - Add dates" />
+      <input type="text" placeholder="Who - Add guests" />
+      <button id="search-btn">
+        Search
+      </button>
     </div>
   );
 };
